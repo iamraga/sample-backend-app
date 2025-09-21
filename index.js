e20c503cb39dae4bca76921d66b597d0cbfbed45
Raga <imraga@gmail.com>
2025-09-21 12:00:32 +0530

    Backend changes

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express = require('express')
+const cors = require('cors');
 const app = express()
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.get('/', (req, res) => {
-  console.log("In console");
+  console.log("In console");x
   res.send('Hello slash!')
 })
 
 app.get('/chicken', (req, res) => {
   console.log("In console");
-  res.send('Hello!')
+  res.send({name: "EMC 20"})
 })
 
 app.listen(port, () => {
